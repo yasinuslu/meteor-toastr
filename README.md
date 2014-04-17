476ed73ebbdb09e69ee22034e52d35b777602ec5
Chris Beckett <chris@beckett.name>
2014-04-17 00:11:18 +0000

    Created package

---
+++ README.md
@@ -0,0 +1,11 @@
+# toastr
+
+[toastr](http://codeseven.github.io/toastr/) JavaScript library for Gnome / Growl type non-blocking notifications, packaged for [Meteor](http://meteor.com).
+
+## Installation
+
+With [Meteorite](https://github.com/oortcloud/meteorite) installed:
+
+```sh
+$ mrt add toastr
+```
---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  "summary": "JavaScript library for Gnome / Growl type non-blocking notifications."
+});
+
+Package.on_use(function (api) {
+  
+  api.use("jquery", "client");
+
+  api.add_files("lib/toastr.min.js", "client");
+  api.add_files("lib/toastr.min.css", "client");
+  
+});
